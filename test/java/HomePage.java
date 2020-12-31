ca7502d4693b62034ba7727a68f4295efb54471a
t3780 <61010902+t3780@users.noreply.github.com>
2020-12-31 18:07:49 +0100

    fout opgemerkt

--- test/java/HomePage.java
+++ test/java/HomePage.java
@@ -1,18 +1,24 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends Page{
 
-    /**
-     * Door Thomas Van Gansen En Lennert Nowak
-     */
-
     @FindBy(id="welcome")
     WebElement paragraaf;
 
     @FindBy(id="logout")
     WebElement button;
+
+    @FindBy(id="login")
+    WebElement loginbutton;
+
+    @FindBy(id="userid")
+    WebElement userid;
+
+    @FindBy(id="password")
+    WebElement password;
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -21,7 +27,15 @@ public class HomePage extends Page{
 
     public boolean isLoggedIn(){
         String tekst = paragraaf.getText();
-        return tekst.contains("welcome");
+        return tekst.contains("hallo");
+    }
+
+    public void logIn(String userId, String pass){
+        userid.clear();
+        userid.sendKeys(userId);
+        password.clear();
+        password.sendKeys(pass);
+        loginbutton.click();
     }
 
     public String getTekst(){
@@ -32,5 +46,17 @@ public class HomePage extends Page{
         button.click();
     }
 
+    public void refreshPage(){
+        driver.navigate().refresh();
+    }
+
+    public void goToOtherPage(){
+        driver.get(path+"Controller?command=Overview");
+    }
+
+    public void goBackToHome(){
+        driver.get(path+"Controller?command=Login");
+    }
 }
+
 
--- test/java/Story4Test.java
+++ test/java/Story4Test.java
@@ -76,3 +76,4 @@ public class Story4Test {
     }
 }
 
+
